6f4a85e97efd8c5de9e8f995b5f696220e044bf8
matteo2710 <156590802+matteo2710@users.noreply.github.com>
2024-03-25 12:20:37 +0100

    main

---
+++ MainChatClient.java
@@ -0,0 +1,6 @@
+
+public class MainChatClient{ 
+public static void main(String[] args) {
+    ChatClient s = new ChatClient();
+}
+}
---
+++ MainChatServer.java
@@ -0,0 +1,6 @@
+
+public class MainChatServer{ 
+public static void main(String[] args) {
+    ChatServer s = new ChatServer();
+}
+}
